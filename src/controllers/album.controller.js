0acdb561e64946feff0514ad7aa995a959e0ed24
Suvam Adhikary <adhikary.saheb408@gmail.com>
2022-01-17 19:24:50 +0530

    update genre controller and added debounce controller for album

--- src/controllers/album.controller.js
+++ src/controllers/album.controller.js
@@ -10,22 +10,28 @@ router.post("", curdController.post(Album));
 router.patch("/:id", curdController.updateOne(Album));
 router.delete("/:id", curdController.deleteOne(Album));
 
+
+
+// GET ALL ALBUMS
+
 router.get("", async (req, res) => {
+  const genres = req.params.genre.split("+");
+  const sort = req.params.sort;
   const page = +req.query.page || 1;
   const size = +req.query.limit || 5;
   const offset = (page - 1) * size;
 
   try {
-    const data = await Album
-                          .find({})
-                          .skip(offset)
-                          .limit(size)
-                          .populate({
-                            path: "artists",
-                            select: "name profilePic"
-                          })
-                          .lean()
-                          .exec();
+    const data = await Album.find({})
+      // .sort(sort || 0)
+      .skip(offset)
+      .limit(size)
+      .populate({
+        path: "artists",
+        select: "name profilePic",
+      })
+      .lean()
+      .exec();
 
     const pages = Math.ceil(
       (await Album.find({}).countDocuments().lean().exec()) / size
@@ -36,6 +42,11 @@ router.get("", async (req, res) => {
     return res.status(400).send({ err });
   }
 });
+
+
+
+
+// GET A ALBUM BY ID
 
 router.get("/:id", async (req, res) => {
   try {
@@ -51,5 +62,26 @@ router.get("/:id", async (req, res) => {
     return res.status(400).json({ ERR: err.message });
   }
 });
+
+
+
+// DEBOUNCE
+
+router.get("/api/search", async (req, res) => {
+  try {
+    let q = req.query.q;
+
+    let key = new RegExp(q, 'i');
+
+    const albums = await Album.find({name: {$regex: key}}).select('_id name').lean().exec();
+
+    return res.status(200).json({ albums })
+
+  }
+  catch (err) {
+    return res.status(400).json({ ERR: err.message });
+  }
+})
+
 
 module.exports = router;
